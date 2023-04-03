fee2dfcfa944af83add0eb1c9ad4e37307757371
gabe927 <gabe927@gmail.com>
2023-04-02 23:56:46 -0400

    de-coupled parsing and processing, fixed getMult

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('data.xml')
+tree = ET.parse('src/data.xml')
 root = tree.getroot()
 
 insts = {}
@@ -42,18 +42,30 @@ class circuit():
             totalLoad += i.load
         return totalLoad
 
+
+### Processing Funcitons ###
+
 # gets the mult class base on mult name, creates a new one if doesn't exist
 def getMultClass(name: str) -> mult:
-    if j.text not in mults:
+    if name is None or name == "":
+        return None
+
+    if name not in mults:
         m = mult()
-        m.name = j.text
-        mults.update({j.text:m})
+        m.name = name
+        mults.update({name:m})
         return m
     else:
-        return mults[j.text]
+        return mults[name]
 
 # gets the circuit class based on mult class and the circuit number, creates a new one if doesn't exist
 def getCircuitClass(m: mult, cirNum: int) -> circuit:
+    if type(cirNum) is str:
+        cirNum = int(cirNum)
+
+    if m is None or cirNum <= 0 or cirNum >= 7:
+        return None
+
     c = m.circuits[cirNum]
     if c == None:
         c = circuit()
@@ -64,7 +76,30 @@ def getCircuitClass(m: mult, cirNum: int) -> circuit:
 
 def addLiToCircuit(li: lightInst, c: circuit) -> None:
     li.circuit = c
-    c.lightInsts.append(li)
+    if type(c) is circuit:
+        c.lightInsts.append(li)
+
+def convert_wattage_to_int(wattage_str):
+    """
+    Converts wattage in the form of a string to an integer.
+    Example input: "1700W", "300 W", "1.7kW", "2.3 kW", "500", "", None
+    Example output: 1700, 300, 1700, 2300, 500, 0, 0
+    """
+    if wattage_str is None or wattage_str == "":
+        return 0
+    
+    wattage_str = wattage_str.strip().lower()
+    wattage_int = None
+    if wattage_str.isdigit():
+        wattage_int = int(wattage_str)
+    elif wattage_str.endswith('w'):
+        wattage_int = int(wattage_str[:-1].strip())
+    elif wattage_str.endswith('kw'):
+        wattage_int = int(float(wattage_str[:-2].strip()) * 1000)
+    return wattage_int
+
+
+### Dump Functions ###
 
 def dumpCircuit(c: circuit, prefix=""):
     print(prefix + "Circuit " + str(c.number))
@@ -85,21 +120,33 @@ def dump(prefix="") -> None:
 instData = root[1]
 for i in reversed(instData):
     if i.tag[:3] == "UID":
-        li = lightInst()
+        UID = None
+        AppStamp = None
+        CircuitName = None
+        CircuitNum = None
+        Wattage = None
+
         for j in i:
             if j.tag == "UID":
-                li.UID = j.text
-            elif j.tag == "Circuit_Name" and j.text != None:
-                li.mult = getMultClass(j.text)
-            elif j.tag == "Circuit_Number" and j.text != None:
-                cirNum = int(j.text)
-                if li.mult == None:
-                    continue
-                c = getCircuitClass(li.mult, cirNum)
-                addLiToCircuit(li, c)
+                UID = j.text
+            elif j.tag == "AppStamp":
+                AppStamp = j.text
+            elif j.tag == "Circuit_Name":
+                CircuitName = j.text
+            elif j.tag == "Circuit_Number":
+                CircuitNum = j.text
+            elif j.tag == "Wattage":
+                Wattage = j.text
 
-        insts.update({li.UID:li})
-        instData.remove(i)
+        if AppStamp == "Vectorworks":
+            li = lightInst()
+            li.UID = UID
+            li.load = convert_wattage_to_int(Wattage)
+            li.mult = getMultClass(CircuitName)
+            addLiToCircuit(li, getCircuitClass(li.mult, CircuitNum))
+            insts.update({li.UID:li})
+
+        instData.remove(i)        
 
 print(insts)
 print("****____****____****____****")
